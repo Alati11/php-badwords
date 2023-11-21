8ed873cf6acba1ea670394b54337365bf1037035
Alati11 <peppealati@gmail.com>
2023-11-21 17:13:48 +0100

    add file word-php

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-    $words = 'Giuseppe';
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form</title>
+</head>
+<body>
+    <h1>Parola censurata</h1>
+    <h4>Scegli la parola</h4>
+  <form action="word.php" method="POST">
+    <input type="text" name="word" placeholder="scegli la parola"> 
+    <input type="submit" value="Invia">
+  </form>
+</body>
+</html>
 
-    var_dump($words);
+<?php
 
-?>    
-    <h1>
-        Ciao PHP!
-    </h1>
-    <p>Mi chiano <?php echo $words; ?></p>
-
-
-
+// var_dump([1,2,3]);
+?>
---
+++ word.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$bad_word = $_POST ['word'];
+
+var_dump($_POST);
+
+?>
+
+<h2>La parola da censurare è <?php echo $bad_word; ?></h2>
